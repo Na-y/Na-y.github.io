5ac81c5f71a580d2153ab40f937cf2912e02cdbd
nay <95525703+Na-y@users.noreply.github.com>
2021-12-15 06:20:50 +0630

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,9 @@
+let site_detail = {
+    'site_name': 'Novel4fun',
+    'tagline': 'Fantasy novels for you',
+    'site_home': 'https://na-y.github.io/',
+    'blog_home': 'https://na-y.github.io/',
+    'products': 'products.html'
+}
 
+export {site_detail}
