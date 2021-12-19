aba767f8c0ea19f1cff38c602d0020d735caef82
nay <95525703+Na-y@users.noreply.github.com>
2021-12-19 16:37:17 +0630

    Delete index.js

--- js/index.js
+++
@@ -1,6 +0,0 @@
-import { openMenu, closeMenu } from "https://na-y.github.io/js/menu.js";
-openMenu(); closeMenu();
-
-import { articles } from "https://na-y.github.io/js/articles.js";
-let blog = 'day2tutorial'; // https://day2tutorial.blogspot.com/
-articles(blog);
